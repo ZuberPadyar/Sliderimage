473443be236320fa3038428d76673c2eba1a848c
Zuber Padyar <juberpadyar111@gmail.com>
2023-05-17 16:34:07 +0530

    change transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import { AnimatePresence, motion } from 'framer-motion'
-import image1 from './components/image1.jpg'
-import image2 from './components/image2.jpg'
-import image3 from './components/image3.jpg'
-import image4 from './components/image4.jpg'
-import image5 from './components/image5.jpg'
+// import image1 from './components/image1.jpg'
+// import image2 from './components/image2.jpg'
+// import image3 from './components/image3.jpg'
+// import image4 from './components/image4.jpg'
+// import image5 from './components/image5.jpg'
 
 const images = [
   'https://images.unsplash.com/photo-1658171757201-41b9aa2b3651?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
@@ -19,7 +19,7 @@ const images = [
   'https://images.unsplash.com/photo-1658901097893-cfe9605ca208?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1192&q=80',
   'https://images.unsplash.com/photo-1659039288596-45cf0fbaee51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   'https://images.unsplash.com/photo-1658730335794-c5edd544ccbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-  image1,image2,image3,image4,image5
+  // image1,image2,image3,image4,image5
 ]
 
 const variants = {
@@ -76,7 +76,29 @@ function App() {
     setIndex(index - 1)
   }
 
+
+  const [x,setx]=useState(0)
+  const [y,sety]=useState(0)
+
+  useEffect(() => {
+  const handleCursor = (e) => {
+    setx(e.clientX);
+    sety(e.clientY);
+  };
+
+  window.addEventListener('mousemove', handleCursor);
+
+  return () => {
+    window.removeEventListener('mousemove', handleCursor);
+  };
+}, [x, y]);
+
+
   return (
+    <>
+    <motion.div className="cursor" animate={{x,y}}
+    
+    ></motion.div>
     <div className='container'>
       <div className='slideshow'>
         <AnimatePresence initial={false} custom={direction}>
@@ -91,6 +113,7 @@ function App() {
             key={images[index]}
             custom={direction}
             drag="x"
+            dragListener={false}
             // dragElastic={0.8}
             onDragEnd={(event,info) =>{
               if(event.offsetX>=200){
@@ -99,17 +122,18 @@ function App() {
                 prevStep()
               }
             }}
-          />
+            />
         </AnimatePresence>
 
          <button className='prevButton' onClick={prevStep}>
-          ◀
+           ◀
         </button>
         <button className='nextButton' onClick={nextStep}>
           ▶
         </button>
       </div>
     </div>
+            </>
   )
 }
 
